e355bc710ac6619abbb11fc660bd8a86cbac3d39
Somu <somusahasa4@gmail.com>
2019-05-29 16:34:55 +0530

    update sw file

--- work/service-worker.js
+++ work/service-worker.js
@@ -28,6 +28,7 @@ self.addEventListener('fetch', function(event) {
       .then(function(response) {
         // Cache hit - return response
         if (response) {
+          console.log('response found');
           return response;
         }
         return fetch(event.request);
